94352b32197e6cee7f28ee3e7d4767c266899126
SaifulIslam <msik.milon@gmail.com>
2025-01-30 17:39:09 -0800

    .

--- FuliangMart/settings.py
+++ FuliangMart/settings.py
@@ -36,8 +36,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG")
 
-# ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
-# ALLOWED_HOSTS += os.environ.get("ALLOWED_HOSTS", "").split()
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS += os.environ.get("ALLOWED_HOSTS", "").split()
 
 # # Access the environment variables
 # SECRET_KEY = os.getenv('SECRET_KEY')
