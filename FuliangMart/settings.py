f2706e18ee89bc1b712a95695e5653b9482ee63c
SaifulIslam <msik.milon@gmail.com>
2025-01-30 17:37:54 -0800

    .

--- FuliangMart/settings.py
+++ FuliangMart/settings.py
@@ -24,7 +24,7 @@ from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(os.path.join(BASE_DIR/ ".env", ".env"))
+load_dotenv(os.path.join(BASE_DIR/ ".vnr", ".env"))
 # load_dotenv()
 
 # Quick-start development settings - unsuitable for production
